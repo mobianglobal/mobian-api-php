d96dbeef9714da2e98e5e4d1d88a6b4796901435
Lars Grevelink <lars.grevelink@mobian.global>
2018-09-12 19:48:43 +0200

    Make sure numeric values are represented by %d in the format strings

--- Requests/Funnels/Fetch.php
+++ Requests/Funnels/Fetch.php
@@ -14,7 +14,7 @@ class Fetch extends AbstractRequest
     /**
      * @inheritdoc
      */
-    protected $endpoint = '/funnels/%s';
+    protected $endpoint = '/funnels/%d';
 
     /**
      * Contructor.
